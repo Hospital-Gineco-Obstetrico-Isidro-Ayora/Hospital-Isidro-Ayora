000da94922a598a7ad343ba90b81a1228b6df015
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2023-03-05 20:38:01 -0500

    Fixed: On load window set Home
    Refactor functions that change route and add active class

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ import { router } from "./src/router/index.js";
 
 const init = () => {
   router(window.location.hash);
-
   window.addEventListener("hashchange", () => {
     router(window.location.hash);
   });
