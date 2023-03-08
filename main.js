71b52b598047668e2c6e1c12aaf9c7a173e45547
NicolasB2H <nicolas_omar97@hotmail.com>
2023-03-07 21:20:06 -0500

    hospitañ-2

--- main.js
+++ main.js
@@ -21,3 +21,36 @@ menuToggle.addEventListener("click", function () {
   navbarCollapse.classList.toggle("show");
 });
 
+
+    /*Para desplegar infromación de botones */
+    var btn1 = document.getElementById("btn1");
+    var btn2 = document.getElementById("btn2");
+    var btn3 = document.getElementById("btn3");
+    var info = document.getElementById("info");
+
+    btn1.addEventListener("click", function() {
+    info.innerHTML = "Información del botón 1";
+    info.style.display = "block";
+    });
+
+    btn2.addEventListener("click", function() {
+    info.innerHTML = "Información del botón 2";
+    info.style.display = "block";
+    });
+
+    btn3.addEventListener("click", function() {
+    info.innerHTML = "Información del botón 3";
+    info.style.display = "block";
+    });
+
+    window.addEventListener("resize", function() {
+    if (window.innerWidth < 768) {
+        btn1.style.width = "100%";
+        btn2.style.width = "100%";
+        btn3.style.width = "100%";
+    } else {
+        btn1.style.width = "auto";
+        btn2.style.width = "auto";
+        btn3.style.width = "auto";
+    }
+    });
--- src/controllers/hospital.controller.js
+++ src/controllers/hospital.controller.js
@@ -3,5 +3,6 @@ import view from '../views/hospital.html?raw';
 export default () => {
     const divElement = document.createElement('div');
     divElement.innerHTML = view;
+    
     return divElement;
 };
